00841601ea5c28da1214553d74b963342950769e
Peter Kuma <peterkuma@waveland.org>
2013-02-14 14:30:25 +0100

    Replace CSS with SCSS/compass

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 import os
-from distutils.core import setup, Extension
+import sys
 from glob import glob
+from distutils.core import setup, Extension, Command
+import distutils.command.build
 from Cython.Distutils import build_ext
+import subprocess
+from subprocess import call
+
 
 def find(prefix, path):
     l = []
@@ -10,6 +15,7 @@ def find(prefix, path):
         l.append((os.path.join(prefix, dirpath[len(path):].lstrip('/')), fl))
     return l
 
+
 data_files = [
     ('share/doc/ccloud/', ['README.md']),
 ]
@@ -17,6 +23,35 @@ data_files = [
 data_files += find('share/ccloud/template/', 'template')
 data_files += find('share/ccloud/www/', 'www')
 data_files += find('share/ccloud/colormaps/', 'colormaps')
+
+
+class build(distutils.command.build.build):
+    def run(self):
+        distutils.command.build.build.run(self)
+        self.run_command('build_scss')
+
+class build_scss(Command):
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            with open(os.devnull, 'w') as fp:
+                proc = subprocess.Popen(['scss', '-v'], stdout=fp, stderr=fp)
+                proc.communicate()
+        except OSError:
+            print >>sys.stderr, 'warning: scss not available, will not rebuild stylesheets'
+            return
+        cmd = ['scss', '--compass', '--update', 'ccloud.scss']
+        print ' '.join(cmd)
+        try:
+            call(cmd, cwd='www/css')
+        except OSError as e:
+            print >>sys.stderr, e.strerror
+
 
 setup(
     name='ccloud',
@@ -42,7 +77,11 @@ setup(
         'src/bin/ccserver',
         'src/bin/cchtree-clean',
     ],
-    cmdclass = {'build_ext': build_ext},
+    cmdclass = {
+        'build': build,
+        'build_ext': build_ext,
+        'build_scss': build_scss,
+    },
     ext_modules=[
         Extension('ccloud.algorithms',
                   ['src/ccloud/algorithms.pyx'],
---
+++ www/css/config.rb
@@ -0,0 +1,2 @@
+images_dir = "../img/"
+
