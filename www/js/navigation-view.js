c30f945cc27c1e12e6bae2ed83e3e931a0087071
Peter Kuma <peterkuma@waveland.org>
2013-04-18 18:12:42 +0200

    Remove trailing white space in navigation-view.js

--- www/js/navigation-view.js
+++ www/js/navigation-view.js
@@ -10,7 +10,7 @@ var NavigationView = new Class({
     initialize: function(el, nav) {
         this.el = el;
         this.nav = nav;
-        
+
         this.monthSlider = this.el.querySelector('.nav-month-slider');
         this.daySlider = this.el.querySelector('.nav-day-slider');
         this.progress = this.el.querySelector('.nav-progress');
@@ -18,14 +18,14 @@ var NavigationView = new Class({
         this.progressed = this.el.querySelector('.nav-progressed');
         this.thumb = this.el.querySelector('.nav-thumb');
         this.pointer = this.el.querySelector('.nav-pointer');
-        
+
         this.initProgress();
         this.initMonthSlider(this.nav.profile.origin[0], new Date());
         this.initDaySlider();
-        
+
         this.nav.on('change', this.update.bind(this));
     },
-    
+
     initProgress: function() {
         var update = function(evt) {
             var value = evt.clientX/this.progress.getSize().x;
@@ -36,19 +36,19 @@ var NavigationView = new Class({
             date.setUTCSeconds(Math.floor((value*24*60*60 % 60)));
             this.nav.setCurrent(date);
         }.bind(this);
-        
+
         this.progress.addEventListener('mousedown', function(evt) {
             update(evt);
             window.addEventListener('mousemove', update);
             evt.preventDefault();
             document.body.setStyle('cursor', 'move');
         });
-        
+
         window.addEventListener('mouseup', function() {
             document.body.setStyle('cursor', 'auto');
             window.removeEventListener('mousemove', update);
         });
-        
+
         this.progress.addEventListener('mousemove', function(evt) {
             var y = this.nav.getCurrent().getUTCFullYear();
             var m = this.nav.getCurrent().getUTCMonth();
@@ -56,25 +56,25 @@ var NavigationView = new Class({
             var date = new UTCDate(y, m, d);
             date.increment('second', evt.clientX/this.progress.getSize().x*24*60*60);
             this.pointer.title = date.formatUTC('%H:%M');
-            this.pointer.setStyle('left', evt.clientX); 
+            this.pointer.setStyle('left', evt.clientX);
         }.bind(this), true);
-        
+
         this.updateProgress();
     },
-    
+
     update: function() {
         var now = new Date();
         this.updateMonthSlider(this.nav.profile.origin[0], now, this.nav.getCurrent());
-        this.updateDaySlider(this.nav.profile.origin[0], now, this.nav.getCurrent());    
+        this.updateDaySlider(this.nav.profile.origin[0], now, this.nav.getCurrent());
         this.updateProgress();
     },
-    
+
     initMonthSlider: function(from, to) {
         var y1 = from.getUTCFullYear();
         var y2 = to.getUTCFullYear();
-        
+
         var w = 0;
-        
+
         for (var y = y1; y <= y2; y++) {
             label = document.createElement('div');
             label.addClass('nav-year');
@@ -82,14 +82,14 @@ var NavigationView = new Class({
             label.store('year', y);
             new Tooltip(label);
             this.monthSlider.appendChild(label);
-            
+
             var months = document.createElement('div');
             months.addClass('nav-months');
             this.monthSlider.appendChild(months);
-            
+
             var container = document.createElement('div');
             months.appendChild(container);
-            
+
             for (var m = y == y1 ? from.getUTCMonth() : 0; m < 12; m++) {
                 label = document.createElement('a');
                 label.addClass('nav-label');
@@ -105,7 +105,7 @@ var NavigationView = new Class({
             months.setStyle('width', 0);
             container.setStyle('width', 10000);
         }
-        
+
         this.accordion = new Fx.Accordion($$('.nav-year'), $$('.nav-months'), {
             display: -1,
             opacity: false,
@@ -118,10 +118,10 @@ var NavigationView = new Class({
                 el.tween('width', 0);
             }
         });
-        
+
         this.updateMonthSlider();
     },
-    
+
     initDaySlider: function() {
         for (var d = 1; d <= 31; d++) {
             label = document.createElement('a');
@@ -132,18 +132,18 @@ var NavigationView = new Class({
             new Tooltip(label);
             this.daySlider.appendChild(label);
         }
-        
+
         this.updateDaySlider();
     },
-    
+
     updateMonthSlider: function() {
         var year = this.nav.getCurrent().getUTCFullYear();
         var month = this.nav.getCurrent().getUTCMonth();
-        
+
         var i = 0;
         this.monthSlider.getElements('.nav-year').each(function(e) {
             var y = e.retrieve('year').toInt();
-            
+
             if (!this.nav.isAvailableYear(y)) {
                 e.addClass('nav-unavailable');
                 e.title = 'Not available';
@@ -151,16 +151,16 @@ var NavigationView = new Class({
                 e.removeClass('nav-unavailable');
                 e.title = '';
             }
-                
+
             if (e.get('text') == year.toString())
                 this.accordion.display(i);
             i++;
         }.bind(this));
-        
+
         this.monthSlider.getElements('.nav-month').each(function(e) {
             var y = e.retrieve('year').toInt();
             var m = e.retrieve('month').toInt();
-            
+
             if (!this.nav.isAvailableMonth(y, m )) {
                 e.addClass('nav-unavailable');
                 e.title = 'Not available';
@@ -168,25 +168,25 @@ var NavigationView = new Class({
                 e.removeClass('nav-unavailable');
                 e.title = '';
             }
-            
+
             if (e.retrieve('year') == year.toString() &&
                 e.retrieve('month') == month.toString())
-            {    
+            {
                 e.addClass('nav-active');
             } else {
                 e.removeClass('nav-active');
             }
         }.bind(this));
     },
-    
+
     updateDaySlider: function(from, to, current) {
         var y = this.nav.getCurrent().getUTCFullYear();
         var m = this.nav.getCurrent().getUTCMonth();
         var day = this.nav.getCurrent().getUTCDate();
-        
+
         this.daySlider.getElements('.nav-day').each(function(e) {
             var d = e.retrieve('day').toInt();
-            
+
             if (!this.nav.isAvailableDay(y, m, d)) {
                 e.addClass('nav-unavailable');
                 e.title = 'Not available';
@@ -194,14 +194,14 @@ var NavigationView = new Class({
                 e.removeClass('nav-unavailable');
                 e.title = '';
             }
-            
+
             e.href = new Date(y, m, d).format('#%Y-%b-%d');
-            
+
             if (d == day)
                 e.addClass('nav-active');
             else
                 e.removeClass('nav-active');
-            
+
             if (d > new Date(y, m).getLastDayOfMonth())
                 e.setStyle('display', 'none');
             else
@@ -217,14 +217,14 @@ var NavigationView = new Class({
         value /= 24*60*60;
         this.progressed.setStyle('width', value*100 + '%');
         this.thumb.setStyle('left', value*100 + '%');
-        
+
         var y = this.nav.getCurrent().getUTCFullYear();
         var m = this.nav.getCurrent().getUTCMonth();
-        var d = this.nav.getCurrent().getUTCDate();    
+        var d = this.nav.getCurrent().getUTCDate();
         var start = new UTCDate(y, m, d);
         var end = new UTCDate(y, m, d).increment('day', 1);
         var intervals = this.nav.availableBetween(start, end);
-        
+
         this.availability.innerHTML = '';
         intervals.forEach(function(interval) {
             var box = document.createElement('div');
@@ -232,7 +232,7 @@ var NavigationView = new Class({
             box.setStyle('width', 100*(interval[1]-interval[0])/(24*60*60*1000) + '%');
             this.availability.appendChild(box);
         }.bind(this));
-        
+
         this.thumb.title = this.nav.getCurrent().formatUTC('%H:%M');
         if (this.thumb.tooltip) this.thumb.tooltip.update();
     }
