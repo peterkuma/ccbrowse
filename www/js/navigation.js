2bcca92e674ea857f3167ceeb8b3435e01338763
Peter Kuma <peterkuma@waveland.org>
2015-10-25 18:24:20 +0100

    Remove trailing spaces in navigation.js

--- www/js/navigation.js
+++ www/js/navigation.js
@@ -18,7 +18,7 @@ export default class Navigation extends EventEmitter {
         window.addEventListener('hashchange', this.update.bind(this));
         this.update();
     }
-    
+
     update() {
         if (window.location.hash === '') return;
         var date = new Date().parse(window.location.hash.substring(1) + ' +0000');
@@ -26,9 +26,9 @@ export default class Navigation extends EventEmitter {
         this.current = date;
         this.emit('change');
     }
-    
+
     getLayers() { return this.profile.layers; }
-    
+
     getLayer() { return this.layer; }
 
     async setLayer(name) {
@@ -36,7 +36,7 @@ export default class Navigation extends EventEmitter {
         this.emit('change');
         this.emit('layerchange');
     }
-    
+
     getCurrent() {
         return new Date(this.current);
     }
@@ -46,7 +46,7 @@ export default class Navigation extends EventEmitter {
         window.location.replace('#'+date.formatUTC('%Y-%b-%d,%H:%M:%S'));
         this.emit('change');
     }
-    
+
     getZoom() {
         return this.zoom;
     }
@@ -55,26 +55,26 @@ export default class Navigation extends EventEmitter {
         this.zoom = zoom;
         this.emit('change');
     }
-    
+
     getMaxZoom() {
         var i = 0;
         while (this.profile.zoom[i.toString()])
             i++;
         return i-1;
     }
-    
+
     getAvailability() {
         if (!this.layer || !this.layer.availability || !this.layer.availability[this.zoom])
             return [];
         return this.layer.availability[this.zoom];
     }
-    
+
     isAvailable(start, end) {
         var availability = this.getAvailability()
-        
+
         var x1  = (start - this.profile.origin[0])/this.profile.zoom[this.zoom].width;
         var x2  = (end - this.profile.origin[0])/this.profile.zoom[this.zoom].width;
-        
+
         for (var i = 0; i < availability.length; i++) {
             var range = availability[i];
             if (range[0] >= x1 && range[0] <= x2) return true;
@@ -83,37 +83,37 @@ export default class Navigation extends EventEmitter {
         }
         return false;
     }
-    
+
     isAvailableYear(year) {
         return this.isAvailable(new UTCDate(year, 0, 1),
                                 new UTCDate(year, 0, 1).increment('year', 1));
     }
-    
+
     isAvailableMonth(year, month) {
         return this.isAvailable(new UTCDate(year, month, 1),
                                 new UTCDate(year, month, 1).increment('month', 1));
     }
-    
+
     isAvailableDay(year, month, day) {
         return this.isAvailable(new UTCDate(year, month, day),
                                 new UTCDate(year, month, day).increment('day', 1));
     }
-    
+
     availableBetween(start, end) {
         if (!this.layer || !this.layer.availability || typeof this.layer.availability == 'string' || !this.layer.availability[this.zoom])
             return [];
         var availability = this.layer.availability[this.zoom];
-        
+
         var x1  = (start - this.profile.origin[0])/this.profile.zoom[this.zoom].width;
         var x2  = (end - this.profile.origin[0])/this.profile.zoom[this.zoom].width;
-        
+
         var intervals = [];
         for (var i = 0; i < availability.length; i++) {
             var range = availability[i];
-            
+
             var date1 = (new Date(this.profile.origin[0])).increment('ms', range[0]*this.profile.zoom[this.zoom].width);
             var date2 = (new Date(this.profile.origin[0])).increment('ms', range[1]*this.profile.zoom[this.zoom].width);
-            
+
             if (range[0] <= x1 && range[1] >= x2) intervals.push([start, end]);
             if (range[0] >= x1 && range[1] <= x2) intervals.push([date1, date2]);
             if (range[0] <= x1 && range[1] >= x1) intervals.push([start, date2]);
