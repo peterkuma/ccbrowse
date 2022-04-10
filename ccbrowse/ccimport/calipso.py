7aee2800120cf7318288d33d8770f80e690c2af3
Peter Kuma <peter@peterkuma.net>
2022-04-10 15:39:30 +0100

    Fix whitespace in multiple Python files

--- ccbrowse/ccimport/calipso.py
+++ ccbrowse/ccimport/calipso.py
@@ -20,18 +20,18 @@ class Calipso(Product):
         'longitude': ['Longitude'],
         'trajectory': ['Latitude', 'Longitude'],
     }
-    
+
     LIDAR_ALTITUDES = calipso_constants.LIDAR_ALTITUDES
     MET_DATA_ALTITUDES = calipso_constants.MET_DATA_ALTITUDES
 
     def __init__(self, filename, profile, offset=None):
         Product.__init__(self, filename, profile, offset)
         self.hdf = HDF(filename)
-    
+
     def layers(self):
         layers = list(self.profile['layers'].keys())
         return set(layers).intersection(list(self.DATASETS.keys()))
-        
+
     def xrange(self, layer, level):
         w = self.profile['zoom'][level]['width']
         time = self.hdf['Profile_UTC_Time']
@@ -46,13 +46,13 @@ class Calipso(Product):
         # One-dimensional layer.
         if self.profile['layers'][layer]['dimensions'] == 'x':
             return [0]
-        
+
         # Two-dimensional layer.
         h = self.profile['zoom'][level]['height']
         z1 = int(math.floor((self.LIDAR_ALTITUDES[-1] - self.profile['origin'][1]) / h))
         z2 = int(math.floor((self.LIDAR_ALTITUDES[0] - self.profile['origin'][1]) / h))
         return list(range(z1, z2+1))
-    
+
     def tile(self, layer, level, x, z):
         #
         #    z ^
@@ -70,18 +70,18 @@ class Calipso(Product):
         #      |     |                                        |
         #  m0  +-----+-----------+---------+------------------+------------->
         #           t0          t1 (n1)   t2 (n2)            tn (nn)       t
-        
+
         tile = {
             'layer': layer,
             'zoom': level,
             'x': x,
             'z': z,
         }
-        
+
         datasets = [self.hdf[name] for name in self.DATASETS[layer]]
         dataset = datasets[0]
         height = self.LIDAR_ALTITUDES
-        
+
         w = self.profile['zoom'][level]['width']
         h = self.profile['zoom'][level]['height']
         t_origin = self.profile['origin'][0]
@@ -106,7 +106,7 @@ class Calipso(Product):
         m1 = ccbrowse.utils.coerce(m1, m0, mm-1)
         m2 = len(height) - np.searchsorted(height[::-1], z1) + 1
         m2 = ccbrowse.utils.coerce(m2, m0, mm-1)
-        
+
         # Trajectory - special case.
         if layer == 'trajectory':
             raw_data_lat = datasets[0][n1_:n2_, 0]
@@ -124,12 +124,12 @@ class Calipso(Product):
                     'type': 'Feature',
                     'geometry': {
                         'type': 'LineString',
-                        'coordinates': list(zip(lon, lat))                        
+                        'coordinates': list(zip(lon, lat))
                     },
-                }]      
+                }]
             }
             return tile
-        
+
         # One-dimensional layer.
         if self.profile['layers'][layer]['dimensions'] == 'x':
             raw_data = dataset[n1_:n2_, 0]
@@ -137,19 +137,19 @@ class Calipso(Product):
                                      np.arange(n1_, n2_, dtype=np.float32),
                                      raw_data).astype(np.float32).reshape(1, 256)
             return tile
-        
+
         # Two-dimensional layer.
         raw_data = dataset[n1_:n2_,m1:m2]
         interpolation = self.profile['layers'][layer].get('interpolation', 'smart')
-        
+
         raw_data = np.ma.masked_array(raw_data)
         raw_data.set_fill_value(np.nan)
-        
+
         try:
             fillvalue = dataset.attributes['fillvalue']
-            raw_data = np.ma.masked_equal(raw_data, fillvalue, copy=False)    
+            raw_data = np.ma.masked_equal(raw_data, fillvalue, copy=False)
         except KeyError as IndexError: pass
-        
+
         if interpolation == 'smart':
             N = np.arange(n1, n2, (n2-n1)/256.0)
             N = np.round(N).astype(np.int) - n1_
@@ -161,13 +161,13 @@ class Calipso(Product):
                             N.astype(np.float32),
                             M.astype(np.float32))
             tile['data'] = data.T.copy()
-        
+
         elif interpolation == 'nearest':
             N = np.arange(n1, n2, (n2-n1)/256.0)
             N = np.round(N).astype(np.int) - n1_
             N = np.ma.masked_outside(N, 0, raw_data.shape[0]-1, copy=False)
             N.set_fill_value(0)
-            
+
             f = interp1d(height[m1:m2][::-1], np.arange(m1, m2)[::-1], 'nearest',
                          bounds_error=False, fill_value=m1-1)
             M = f(np.arange(z1, z2, (z2-z1)/256.0)[::-1])
@@ -181,19 +181,19 @@ class Calipso(Product):
             mesh = np.meshgrid(N.filled(), M.filled())
             data = np.ma.masked_where(mask, raw_data[mesh], copy=False)
             tile['data'] = data.filled(np.nan)
-        
+
         else:
             raise RuntimeError('Unknown interpolation %s' % interpolation)
-        
+
         return tile
-    
+
     def _dt2ms(self, td):
         return 1.0 * (td.microseconds + (td.seconds + td.days * 24 * 3600) * 10**6) / 10**3
-    
+
     def _time2dt(self, time):
         """Convert time, a floating-point number in format yymmdd.ffffffff
         to a instance of python datetime class."""
-        
+
         d = int(time % 100)
         m = int((time-d) % 10000)
         y = int(time-m-d)
